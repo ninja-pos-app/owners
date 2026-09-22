8e107075a944bb981e0a0a27b9153ee1d5d4f73c
ninja-pos-app <ninja-pos-app@users.noreply.github.com>
2026-09-22 20:01:41 +0300

    Show the logo instead of a letter

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'owners-4ec7d48552';
+const CACHE = 'owners-50e347fd12';
 const SHELL = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)).then(() => self.skipWaiting())); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(k => Promise.all(k.filter(n => n !== CACHE).map(n => caches.delete(n)))).then(() => self.clients.claim())); });
