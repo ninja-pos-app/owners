1388d2d788ce2783594c41a52a61bf7de9f23e35
ninja-pos-app <ninja-pos-app@users.noreply.github.com>
2026-09-24 16:26:19 +0300

    Five tabs: Reports with monthly periods, Stock and invoices merged, clearer payroll message

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'owners-fe243a91a9';
+const CACHE = 'owners-fe25103ff4';
 const SHELL = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)).then(() => self.skipWaiting())); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(k => Promise.all(k.filter(n => n !== CACHE).map(n => caches.delete(n)))).then(() => self.clients.claim())); });
